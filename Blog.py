271a51a0cd948285a792cf3c46f80e49f54c75e4
samwanyua <samexample8@gmail.com>
2024-04-09 13:00:45 +0300

    Getting started with Flask

--- Blog.py
+++ Blog.py
@@ -1,15 +1,30 @@
-from flask import Flask # importing Flask class
+from flask import Flask, render_template# importing Flask class
 app = Flask(__name__) #instantiated flask application
+
+posts = [
+    {
+        'author': 'Sam Wanyua',
+        'title': 'The new Earth',
+        'content': 'Have you had the new earth that is coming',
+        'date-posted': 'April 20, 2023'
+    },
+    {
+        'author': 'Jetlifer Ikioe',
+        'title': 'The arise of Cybertrucks',
+        'content': '$3453 new cybertrucks',
+        'date-posted': 'June 20, 2023'
+    }
+]
 
 @app.route("/") # home page
 @app.route("/home")
 def home():
-    return "<h2>Home page!</h2>"
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
 def about():
-    return "<h2>About page!</h2>"
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
